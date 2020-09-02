2f77c9c01b856776f699af12117d193abd95a396
Mohamed Saleh <mohamed_andela@medable.com>
2020-09-03 00:02:07 +0200

    test 3

--- app/src/main/java/com/mohamed/myapplication/MainActivity.java
+++ app/src/main/java/com/mohamed/myapplication/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("test", "1");
         Log.d("test", "2");
+        Log.d("test", "3");
     }
 }
